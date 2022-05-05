edee6f900275267532383a2a27d417e6705e03a9
Cata Olivar <cataolivar@gmail.com>
2022-05-05 19:37:57 -0300

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,7 @@
+let input1  =  document.getElementById ("nombre");
+let input2  = document.getElementById ("mail");
+let input3 = document.getElementById ("enviar");
+
+input1.onchange = () => console.log("valor1");
+input2.onchange = () => console.log("valor2");
+input3.onmousemove = () => console.log ("movio")
